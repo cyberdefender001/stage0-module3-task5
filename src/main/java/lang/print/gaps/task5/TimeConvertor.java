4939508ece5c1b9f92b9818f7f89e6e6ef45f71d
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-01 14:53:32 +0500

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void convert(float minutes) {
+        int seconds = minutes * 60;
+        System.out.println(seconds);
+    }
+    public static void main(String[] args) {
+        convert(3);
     }
 }
