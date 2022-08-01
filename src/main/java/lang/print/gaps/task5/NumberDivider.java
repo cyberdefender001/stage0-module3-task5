35f8a1236cab535bfe0b9cdcdce2512ee984f993
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-08-01 14:49:45 +0500

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    public static void divide(int toBeDivided) {
+        double result = toBeDivided / 5;
+        System.out.println(result);
+    }
+    public static void main(String[] args){
+        divide(10);
     }
 }
+
